87ab571d8b39647f812f9e757d2b6cd44c5a1eb5
Evyatar Haim <evyataros1@gmail.com>
2021-01-29 01:57:38 +0200

    adding react-navigation dependencies

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Signal</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
