2ada2aef58636ce285faf6d720346694a106442f
Evyatar Haim <evyataros1@gmail.com>
2021-01-31 19:13:23 +0200

    adding a navigation root Register and navigate this screen by pressing the Register button

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
 
 const Stack = createStackNavigator();
 
@@ -17,10 +19,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={globalScreenOptions}>
-      <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-      />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, View, Text } from 'react-native';
 import { Button, Input, Image } from 'react-native-elements';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
 
    const [ email, setEmail ] = useState("");
    const [ password, setPassword ] = useState("");
 
    const signIn = () => {
-       
+
    }
 
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light"/>
            <Image source={{
                uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
@@ -38,15 +38,27 @@ const LoginScreen = () => {
            </View>
 
            <Button containerStyle={styles.button} title="Login" onPress={signIn} />
-           <Button containerStyle={styles.button} type="outline" title="Register" />
-        </View>
+           <Button containerStyle={styles.button} type="outline" title="Register" onPress={() => navigation.navigate('Register')} />
+           <View style={{height: 100 }} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 10,
+      backgroundColor: "white",
+    },
     inputContainer:{
-
+      width: 300,
+    },
+    button:{
+       width: 200,
+       marginTop: 10
     },
 });
