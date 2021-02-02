9981c789b84a4c2d9d228bcbcb503ddc622ef981
Evyatar Haim <evyataros1@gmail.com>
2021-02-02 23:33:57 +0200

    adding new chat

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,8 @@ import CustomListItem from '../components/CustomListItem';
 import { Avatar } from 'react-native-elements';
 import { auth, db } from '../firebase';
 import { TouchableOpacity } from 'react-native';
+import { AntDesign, SimpleLineIcons} from '@expo/vector-icons';
+
 const HomeScreen = ({ navigation}) => {
 
     const signOutUser = () => {
@@ -24,9 +26,25 @@ const HomeScreen = ({ navigation}) => {
                          <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
                      </TouchableOpacity>
                  </View>
-             )
+             ),
+             headerRight: () => (
+                 <View 
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: "space-between",
+                        width: 80,
+                        marginRight: 20,
+                    }}>
+                        <TouchableOpacity activeOpacity={0.5}>
+                            <AntDesign name="camerao" size={24} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate('AddChat')} activeOpacity={0.5}>
+                            <SimpleLineIcons name="pencil" size={24} color="black" />
+                        </TouchableOpacity>
+                 </View>
+             ),
         });
-     }, [])
+     }, [navigation])
 
     return (
         <SafeAreaView>
